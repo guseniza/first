f2232d708f51ae7b2b90f736689959b2203599c3
introqt <introtheintro@gmail.com>
2016-12-29 20:41:04 +0200

    login and logout

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <?php
 session_start();
+//log in form handler===
+$login = $_POST['login'];
+if(!empty($login)){
+    $_SESSION['login'] = $login;
+}
+//===
+
+//exit
+if(isset($_GET['action'])){
+    session_destroy();
+    header('Location:'.$_SERVER['PHP_SELF']);
+}
+//===
 ?>
 <html>
     <head>
@@ -10,13 +23,15 @@ session_start();
     <body>
         <?php if(empty($_SESSION['login'])):?>
         <form method="post">
-            <label>Login:
+            <label>Log in:
                 <input type="text" name="login"/>
             </label>
             <input type="submit" value="log in"/>
             
         </form>
         <?php else:?>
+            <?= $_SESSION['login'] ?>
+        <a href="?action=exit">Log out</a>
         <?php endif;?>
     </body>
 </html>

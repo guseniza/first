d6f34f81b0a15be499b280d9021ae2163dcf87fd
introqt <introtheintro@gmail.com>
2016-12-29 20:18:32 +0200

    add form login to index.php

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ session_start();
     </head>
     <body>
         <?php if(empty($_SESSION['login'])):?>
+        <form method="post">
+            <label>Login:
+                <input type="text" name="login"/>
+            </label>
+            <input type="submit" value="log in"/>
+            
+        </form>
         <?php else:?>
         <?php endif;?>
     </body>
